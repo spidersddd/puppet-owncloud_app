68f139c4758bb8ed8273c38beef89c65cafa6ea4
Troy Klein <troy.klein@puppetlabs.com>
2016-08-31 15:25:38 -0500

    remove owncloud_app::web for now.

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,20 +27,20 @@ application owncloud_app (
   if (size($web_components) == 0) {
     fail("Found no web component for Owncloud_app[${name}]. At least one is required")
   }
-  # For each of these declare the component and create an array of the exported
-  # Http resources from them for the load balancer.
-  $web_https = $web_components.map |$comp_name| {
-    # Compute the Http resource title for export and return.
-    $http = Http["web-${comp_name}"]
-    # Declare the web component.
-    owncloud_app::web { $comp_name:
-      apache_port => $web_port,
-      interface   => $web_int,
-      consume     => Database["owndb-${name}"],
-      export      => $http,
-    }
-    # Return the $http resource for the array.
-    $http
-  }
+#  # For each of these declare the component and create an array of the exported
+#  # Http resources from them for the load balancer.
+#  $web_https = $web_components.map |$comp_name| {
+#    # Compute the Http resource title for export and return.
+#    $http = Http["web-${comp_name}"]
+#    # Declare the web component.
+#    owncloud_app::web { $comp_name:
+#      apache_port => $web_port,
+#      interface   => $web_int,
+#      consume     => Database["owndb-${name}"],
+#      export      => $http,
+#    }
+#    # Return the $http resource for the array.
+#    $http
+#  }
 
 }
